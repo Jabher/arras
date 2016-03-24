ed907fba03184b5ae58e39a84c92e04ad0c6927d
Vsevolod Rodionov <crbrus@gmail.com>
2016-03-24 13:29:47 +0300

    using webpack renderer for back-end

--- bootstrapper/react-router-middleware.js
+++ bootstrapper/react-router-middleware.js
@@ -1,8 +1,26 @@
+import config from 'config';
 import {match} from 'react-router';
-import routes from '../webapp/routes';
 import render from './util/react-router-middleware-html';
+import webpack from 'webpack';
+import MemoryFS from 'memory-fs';
+import {context, output, module, plugins} from '../webpack.config.babel'
+
+const routesPath = `${config.paths.webappDir}/routes.js`;
+
+const compiler = webpack({
+    context, output, module, plugins, target: 'node',
+    entry: {routes: `./${require('path').relative(context, routesPath)}`}
+});
+compiler.outputFileSystem = new MemoryFS();
+
+const compilerLoad = new Promise(res => compiler.run((err) =>
+    res(err ? null : eval(compiler.outputFileSystem.readFileSync('/routes.js').toString()).default)));
 
 export default async function ReactRouterMiddleware(ctx, next) {
+    const routes = await compilerLoad;
+
+    if (!routes)
+        await next();
     const [error, redirectLocation, renderProps] = await new Promise(res =>
         match({routes, location: ctx.req.url}, (...args) => res(args)));
 
